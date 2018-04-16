88a20b3b7385ec3dcfd413f332dbb67bb604a859
Novalis YAO <kyaonovalis@gmail.com>
2018-04-16 01:28:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+ function add() {
+    var a = 1
+    var b = 80
+  return (a+b)
+}
+
+ function subtract() {
+    var a = 60
+    var b = 40
+  return (a-b)
+}
+
+  function multiply() {
+    var a = 2
+    var b = 3.4
+   return (a*b)
+}
+
+  function divide() {
+    var a = 5
+    var b = 2.5
+   return (a/b)
+}
+
+  function INC(){
+  return inc(a)
+}
+
+  function DEC() {
+   return dec(a)
+}
+
+  function makeInt(n) {
+    return makeInt(a.toString())
+  }
+
+   function preserveDecimal(n){
+   return preserveDecimal('2.222')
+  }
